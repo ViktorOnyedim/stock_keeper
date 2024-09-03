9261d88b70a5cbb1ff81bb1e7a5a3db245fad959
ViktorOnyedim <viktoronyedim@gmail.com>
2024-09-03 06:59:15 +0100

    Modified __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,30 @@
+import os
 from flask import Flask
-from .blueprints import main
-from .blueprints import auth
-from .extensions import db
+from .extensions import db, migrate
+from .blueprints import routes
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 
-app.register_blueprint(main.bp)
-app.register_blueprint(auth.bp)
+# Set secret key for session management
+load_dotenv()
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Initialize extensions
 db.init_app(app)
+migrate.init_app(app, db)
 
-# Create database
-with app.app_context():
-    db.create_all()
+# Register Blueprints
+app.register_blueprint(routes.bp)
+
+if __name__ in "__main__":
+    # Create database
+    with app.app_context():
+        db.create_all()
+    
+    app.run(debug=True)
